6f371d58ab6bdb05c02696e5efee947d6f076326
Pablo <pawel-blukacz@wp.pl>
2025-08-08 12:00:18 +0200

    completed pipeline

--- etl/main.py
+++ etl/main.py
@@ -1,7 +1,9 @@
 from extract import *
 from transform import *
+from load import *
 
 if __name__ == '__main__':
     leki = extract("https://rejestry.ezdrowie.gov.pl/api/rpl/medicinal-products/pharmaceutical-raw-materials/get-xml",
                    "pharm.xml")
     baza = transform(leki)
+    load(baza)
